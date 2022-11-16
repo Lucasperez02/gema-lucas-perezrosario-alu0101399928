5cbc5baf1d8091b9d9a1d3d93392b71746d1c679
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-11-16 15:46:45 +0000

    Expectativas de initialize sin fallos

--- lib/gema_aparcamiento/vehiculo.rb
+++ lib/gema_aparcamiento/vehiculo.rb
@@ -3,6 +3,32 @@ module GemaAparcamiento
     class Vehiculo
         include Comparable
         attr_reader :id, :altura, :anchura, :largo, :peso
+        def initialize(id, altura, anchura, largo, peso)
+            #Excepciones para id
+            if !(id.is_a? Integer) or id < 0
+                raise ArgumentError.new("El valor del id debe ser un int positivo")
+            end
+            #Excepciones para altura
+            if !(altura.is_a? Float) or altura < 0
+                raise ArgumentError.new("El valor de la altura debe ser un float positivo")
+            end
+            #Excepciones para anchura
+            if !(anchura.is_a? Float) or anchura < 0
+                raise ArgumentError.new("El valor de la anchura debe ser un float positivo")
+            end
+            #Excepciones para largo
+            if !(largo.is_a? Float) or largo < 0
+                raise ArgumentError.new("El valor del largo debe ser un float positivo")
+            end
+            #Excepciones para peso
+            if !(peso.is_a? Float) or peso < 0
+                raise ArgumentError.new("El peso debe ser un float positivo")
+            end
+
+            @id = id
+            @altura, @anchura, @largo, @peso = altura, anchura, largo, peso
+
+        end
     end
 
 end
