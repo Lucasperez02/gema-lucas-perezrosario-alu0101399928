25bb7526ef6f7f6f36a82c86fb85ced516ccd2d5
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-11-17 14:40:52 +0000

    Expectativas de los operadores de comparación para clase motor

--- spec/gema_aparcamiento_spec.rb
+++ spec/gema_aparcamiento_spec.rb
@@ -129,6 +129,7 @@ RSpec.describe GemaAparcamiento do
   describe GemaAparcamiento::Motor do
     before (:all) do
       @motor1 = GemaAparcamiento::Motor.new(12334, 1.45, 2.0, 4.3, 700.0,4,5,1700,100)
+      @motor4 = GemaAparcamiento::Motor.new(54321, 1.45, 2.0, 4.3, 700.0,4,5,2000,180)
       @motor2 = GemaAparcamiento::Motor.new(54321, 1.45, 2.0, 4.3, 700.0,4,2,2000,180)
       @motor3 = GemaAparcamiento::Motor.new(96128, 1.45, 2.0, 4.3, 700.0,2,1,1900,180)
     end
@@ -146,13 +147,19 @@ RSpec.describe GemaAparcamiento do
     end
 
     it "Probando la variable de clase de vehiculo" do
-      expect(GemaAparcamiento::Motor.count()).to eq(9)
+      expect(GemaAparcamiento::Motor.count()).to eq(10)
     end
 
     it "Probado to_s de vehiculo a motor" do
       expect(@motor1.to_s()).to eq("Vehiculo con id 12334, altura 1.45 m, anchura 2.0 m, largo 4.3 m y un peso de 700.0 kg. Vehiculo a motor de 4 ruedas, 5 plazas, potencia de motor de 1700 cc y máximo 100 km hora")
       expect(@motor2.to_s()).to eq("Vehiculo con id 54321, altura 1.45 m, anchura 2.0 m, largo 4.3 m y un peso de 700.0 kg. Vehiculo a motor de 4 ruedas, 2 plazas, potencia de motor de 2000 cc y máximo 180 km hora")
       expect(@motor3.to_s()).to eq("Vehiculo con id 96128, altura 1.45 m, anchura 2.0 m, largo 4.3 m y un peso de 700.0 kg. Vehiculo a motor de 2 ruedas, 1 plazas, potencia de motor de 1900 cc y máximo 180 km hora")
+    end
+
+    it "Probando los operadores de comparación de Motor" do
+      expect(@motor1 == @motor4).to eq(true)
+      expect(@motor1 > @motor2).to eq(true)
+      expect(@motor3 < @motor2).to eq(true)
     end
 
   end
