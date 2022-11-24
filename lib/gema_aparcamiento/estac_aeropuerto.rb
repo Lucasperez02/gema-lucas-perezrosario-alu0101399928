b66faf445e5c315400c7bfa488cc1eadd668d73d
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-11-24 00:54:55 +0000

    Expectativas del initialize de EstacAeropuerto sin fallos

--- lib/gema_aparcamiento/estac_aeropuerto.rb
+++ lib/gema_aparcamiento/estac_aeropuerto.rb
@@ -1,7 +1,17 @@
 module GemaAparcamiento
 
-    class EstacAeropuerto < GemaAparcamiento::Datos
-
+    class EstacAeropuerto < GemaAparcamiento::Datos  
+        def initialize(accesibilidad, seguridad, id, nombre_comercial, descripcion, tipo_aparcamiento, plazas, plazas_ocupadas, distancia, plazas_minusvalidos, precio_x_minuto, cjto_vehiculos, n_plantas)
+            if !(n_plantas.is_a? Integer) or n_plantas < 0
+                raise ArgumentError.new("El n_plantas debe ser un int positivo")    
+            end
+            if n_plantas > plazas
+                raise ArgumentError.new("El n_plantas debe ser < que le n_plazas totales")
+            end
+            super(accesibilidad, seguridad, id, nombre_comercial, descripcion, tipo_aparcamiento, plazas, plazas_ocupadas, distancia, plazas_minusvalidos, precio_x_minuto, cjto_vehiculos)
+            @n_plantas = n_plantas
+        end
+        
     end
 
 end
--- spec/estac_aeropuerto_spec.rb
+++ spec/estac_aeropuerto_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe GemaAparcamiento do
             @veh1_1 = GemaAparcamiento::Vehiculo.new(54321, 1.45, 2.0, 4.3, 700.0)
             @veh2_2 = GemaAparcamiento::Vehiculo.new(12344, 1.79, 1.85, 5.26, 1200)
         end
+        
         it "Expectativas del initialize de EstacAeropuerto" do
             expect(GemaAparcamiento::EstacAeropuerto.new(1, 10, 12345, "Mercadona", "Mixto", "coches", 30, 27, 2, 5, 0.5, [@veh1_1, @veh2_2],3)).not_to eq(nil)
             expect{GemaAparcamiento::EstacAeropuerto.new(1, 10, 12345, "Mercadona", "Mixto", "coches", 30, 27, 2, 5, 0.5, [@veh1_1, @veh2_2], -3)}.to raise_error(ArgumentError) #El n_plantas debe ser un int positivo
